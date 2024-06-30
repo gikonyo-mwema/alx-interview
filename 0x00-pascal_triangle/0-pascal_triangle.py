ba9f8a1433a9384e39ea713a76ab4813ee5284ab
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-06-30 21:52:44 +0300

    pascals

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""
-Pascal's Triangle
-"""
-
-
-from math import factorial
+""" Pascal’s triangle"""
 
 
 def pascal_triangle(n):
     """
-    Return pascals triangle using list of lists
+    list of lists of integers representing the Pascal’s triangle of n
     """
-
-    # Return an empty list for non-positive n
-    if n <= 0:
-        return []
-
-    # Initialize an empty list to store the rows
     triangle = []
 
+    if n <= 0:
+        return triangle
     for i in range(n):
-        # Create an empty list for the current row
-        row = []
-
+        temp = []
         for j in range(i+1):
-            # Calculate the binomial cofficient (n choose k)
-            coef = factorial(i) // (factorial(j) * factorial(i - j))
-            # Add the coefficient to the row
-            row.append(coef)
-            # Add the row to the triangle
-        triangle.append(row)
+            if j == 0 or j == i:
+                temp.append(1)
+            else:
+                temp.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp)
 
     return triangle
