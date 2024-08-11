ac8189004db02c5be8d6702d9fb84eccd1ee05ff
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-08-11 12:02:21 +0300

    use of promise

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,54 @@
 #!/usr/bin/node
 
 const request = require('request');
+const movieId = process.argv[2];
 
-if (process.argv.length !== 3) {
+if (!movieId) {
   console.log('Usage: ./0-starwars-characters.js <movie_id>');
   process.exit(1);
 }
 
-const movieId = process.argv[2];
-const baseUrl = 'https://swapi.dev/api/films/';
-const url = `${baseUrl}${movieId}/`;
+// Function to fetch character names
+function fetchCharacterNames (characterUrls) {
+  const promises = characterUrls.map(url => {
+    return new Promise((resolve, reject) => {
+      request(url, (err, response, body) => {
+        if (err) {
+          reject(err);
+        } else if (response.statusCode === 200) {
+          const characterData = JSON.parse(body);
+          resolve(characterData.name);
+        } else {
+          reject(new Error(`Failed to retrieve character data: ${response.statusCode}`));
+        }
+      });
+    });
+  });
 
-request(url, (error, response, body) => {
+  return Promise.all(promises);
+}
+
+// Fetch movie data and then character names
+request(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
   if (error) {
-    console.error('Failed to retrieve data:', error);
+    console.error('Failed to retrieve movie data:', error);
     process.exit(1);
   }
 
   if (response.statusCode === 200) {
     const movieData = JSON.parse(body);
-    const characters = movieData.characters;
-    characters.forEach((characterUrl) => {
-      request(characterUrl, (charError, charResponse, charBody) => {
-        if (charError) {
-          console.error('Failed to retrieve character data:', charError);
-          return;
-        }
-        if (charResponse.statusCode === 200) {
-          const charData = JSON.parse(charBody);
-          console.log(charData.name);
-        }
+    const characterUrls = movieData.characters;
+
+    fetchCharacterNames(characterUrls)
+      .then(names => {
+        names.forEach(name => {
+          console.log(name);
+        });
+      })
+      .catch(err => {
+        console.error('Error fetching character names:', err);
       });
-    });
   } else {
-    console.log(`Failed to retrieve data: ${response.statusCode}`);
+    console.log(`Failed to retrieve movie data: ${response.statusCode}`);
   }
 });
